bbba04a15ff0b0ea13ec293cfcc9ce3bb0c5b93c
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-03-23 20:51:26 +0200

    Add files via upload

---
+++ ffmpeg_177_armv7ahf.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/ffmpeg_177_armv7ahf.ipk "https://raw.githubusercontent.com/Ham-ahmed/233/refs/heads/main/ffmpeg_177_armv7ahf.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/ffmpeg_177_armv7ahf.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "**********************************************************
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo "====================================================="
+############################################                                                                                                                  
+echo ". >>>>         RESTARING         <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
